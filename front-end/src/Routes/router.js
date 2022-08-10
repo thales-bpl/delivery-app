a0fdb83ec449ed0d28f1f9f3eee82f7ac8be7c9c
Matheus Benini <matheus_benini@hotmail.com>
2022-08-10 13:40:22 -0300

    fix: lint

--- front-end/src/Routes/router.js
+++ front-end/src/Routes/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, Redirect ,BrowserRouter } from 'react-router-dom';
+import { Route, Routes, Redirect, BrowserRouter } from 'react-router-dom';
 import Register from '../pages/Register';
 import Login from '../pages/Login';
 
