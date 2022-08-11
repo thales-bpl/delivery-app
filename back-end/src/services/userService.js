b1ec350f9cd35a09444fc15e9d08763a890fb106
Vitor Correa <vitorcorrea18@gmail.com>
2022-08-11 16:17:31 -0300

    chore: debug userService

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -2,7 +2,9 @@ const { User } = require('../database/models');
 const { NOT_FOUND } = require('../error/errorCatalog');
 
 const postLogin = async (email) => {
+  console.log('login service - ', email)
   const login = await User.findAll({ where: { email } });
+  console.log('apos a consulta na model - ',login);
   if (!login) throw NOT_FOUND;
 
   // TO-DO: implementar return
