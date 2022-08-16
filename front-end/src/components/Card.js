c8536ccff123af7b9ac6843041f8a1f3b5122e90
emmanoel1 <emmanoelporto@gmail.com>
2022-08-15 22:52:31 -0300

    fix: test fix img render

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,20 +1,12 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-// import img from '../images/';
 
-export default function CardProduct({ index, price, urlImage, name }) {
+const cervejaBugada = require('../images/antarctica_pilsen_300ml.jpg');
+
+export default function CardProduct({ index, price, name }) {
   const [quantity, setQuantity] = useState(0);
 
-  /*eslint-disable */
-  const img = require(`../${urlImage}`);
-
-  // https://stackoverflow.com/questions/34582405/react-wont-load-local-images
-  
-  // https://stackoverflow.com/questions/27732209/turning-off-eslint-rule-for-a-specific-line
-
-  /*eslint-disable */
-
-  console.log(urlImage);
+  // console.log(a);
 
   const handleChange = (event) => {
     if (event.target.value <= 0) {
@@ -45,7 +37,7 @@ export default function CardProduct({ index, price, urlImage, name }) {
         height="100px"
         width="100px"
         data-testid={ `customer_products__img-card-bg-image-${index}` }
-        src={ img }
+        src={ cervejaBugada }
         alt="Imagem do produto"
       />
       <p data-testid={ `customer_products__element-card-title-${index}` }>
@@ -81,6 +73,6 @@ export default function CardProduct({ index, price, urlImage, name }) {
 CardProduct.propTypes = {
   index: PropTypes.number.isRequired,
   price: PropTypes.string.isRequired,
-  urlImage: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  // urlImage: PropTypes.string.isRequired,
 };
