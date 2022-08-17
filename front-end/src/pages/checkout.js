700b24b91c1ec3dbddaefbb6c31f04d2a950c083
Vitor Correa <vitorcorrea18@gmail.com>
2022-08-17 18:21:58 -0300

    fix: render the nav header

--- front-end/src/pages/checkout.js
+++ front-end/src/pages/checkout.js
@@ -4,6 +4,7 @@ import FinishOrderBtn from '../components/FinishOrderBtn';
 import MainContext from '../store/Context';
 import RemoveBtn from '../components/RemoveBtn';
 import getLocalStorage from '../services/getLocalStorage';
+import Navbar from '../components/Navbar';
 
 export default function Checkout() {
   const [totalPrice, setTotalPrice] = useState('0');
@@ -47,88 +48,91 @@ export default function Checkout() {
   }, [productsCart]);
 
   return (
-    <main className="checkout_main">
-      <h2>Finalizar Pedido</h2>
-      <table className="checkout_produts_section">
-        <thead className="checkout_table_head">
-          <tr>
-            <th>Item</th>
-            <th>Descrição</th>
-            <th>Quantidade</th>
-            <th>Valor Unitário</th>
-            <th>Sub-total</th>
-            <th>Remover Item</th>
-          </tr>
-        </thead>
-        <tbody className="checkout_table_body">
-          {
-            productsCart.map((product, index) => (
-              <tr
-                data-testid={ `element-order-table-name-${index}` }
-                key={ index }
-              >
-                <td>{ index + 1 }</td>
-                <td>{ product.name }</td>
-                <td>{ product.quantity }</td>
-                <td>{ Number(product.price).toFixed(2) }</td>
-                <td>{ (product.price * product.quantity).toFixed(2) }</td>
-                <td>
-                  <RemoveBtn id={ product.id } />
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
-      <aside className="checkout_total_div">{`Total: ${totalPrice}`}</aside>
-      <form>
-        <h2>Detalhes e Endereço para Entrega</h2>
-        <br />
-        <label htmlFor="checkout_seller_option" className="label">
-          P.Vendedora Responsável:
-          <select
-            className="checkout_select"
-            id="checkout_seller_option"
-          >
+    <>
+      <Navbar />
+      <main className="checkout_main">
+        <h2>Finalizar Pedido</h2>
+        <table className="checkout_produts_section">
+          <thead className="checkout_table_head">
+            <tr>
+              <th>Item</th>
+              <th>Descrição</th>
+              <th>Quantidade</th>
+              <th>Valor Unitário</th>
+              <th>Sub-total</th>
+              <th>Remover Item</th>
+            </tr>
+          </thead>
+          <tbody className="checkout_table_body">
             {
-              sellers.map((seller) => (
-                <option
-                  key={ seller.id }
-                  onChange={ () => handleSellerChange(seller.id) }
-                  value={ seller.id }
+              productsCart.map((product, index) => (
+                <tr
+                  data-testid={ `element-order-table-name-${index}` }
+                  key={ index }
                 >
-                  { seller.name }
-                </option>
+                  <td>{ index + 1 }</td>
+                  <td>{ product.name }</td>
+                  <td>{ product.quantity }</td>
+                  <td>{ Number(product.price).toFixed(2) }</td>
+                  <td>{ (product.price * product.quantity).toFixed(2) }</td>
+                  <td>
+                    <RemoveBtn id={ product.id } />
+                  </td>
+                </tr>
               ))
             }
-          </select>
-        </label>
-        <label htmlFor="checkout_adressInput">
-          Endereço
-          <input
-            id="checkout_adressInput"
-            type="text"
-            value={ adressInput }
-            onChange={ ({ target: { value } }) => setAdressInput(value) }
-            placeholder="Travessa Terceira da Castanheira, Bairro Muruci"
-          />
-        </label>
-        <label htmlFor="checkout_adssNumberInput">
-          Número
-          <input
-            id="checkout_adssNumberInput"
-            type="text"
-            value={ adssNumberInput }
-            onChange={ ({ target: { value } }) => setAdssNumberInput(value) }
-            placeholder="198"
-          />
-        </label>
-      </form>
-      <FinishOrderBtn
-        adressInput={ adressInput }
-        adssNumberInput={ adssNumberInput }
-        totalPrice={ totalPrice }
-      />
-    </main>
+          </tbody>
+        </table>
+        <aside className="checkout_total_div">{`Total: ${totalPrice}`}</aside>
+        <form>
+          <h2>Detalhes e Endereço para Entrega</h2>
+          <br />
+          <label htmlFor="checkout_seller_option" className="label">
+            P.Vendedora Responsável:
+            <select
+              className="checkout_select"
+              id="checkout_seller_option"
+            >
+              {
+                sellers.map((seller) => (
+                  <option
+                    key={ seller.id }
+                    onChange={ () => handleSellerChange(seller.id) }
+                    value={ seller.id }
+                  >
+                    { seller.name }
+                  </option>
+                ))
+              }
+            </select>
+          </label>
+          <label htmlFor="checkout_adressInput">
+            Endereço
+            <input
+              id="checkout_adressInput"
+              type="text"
+              value={ adressInput }
+              onChange={ ({ target: { value } }) => setAdressInput(value) }
+              placeholder="Travessa Terceira da Castanheira, Bairro Muruci"
+            />
+          </label>
+          <label htmlFor="checkout_adssNumberInput">
+            Número
+            <input
+              id="checkout_adssNumberInput"
+              type="text"
+              value={ adssNumberInput }
+              onChange={ ({ target: { value } }) => setAdssNumberInput(value) }
+              placeholder="198"
+            />
+          </label>
+        </form>
+        <FinishOrderBtn
+          adressInput={ adressInput }
+          adssNumberInput={ adssNumberInput }
+          totalPrice={ totalPrice }
+        />
+      </main>
+    </>
   );
 }
