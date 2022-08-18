12eef15f74c1dc3081f5084992c54fc3e7e9ba67
Vitor Correa <vitorcorrea18@gmail.com>
2022-08-18 14:11:00 -0300

    fix: remove console.log

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -28,7 +28,6 @@ export default function Products() {
       .toFixed(2);
     const totalPriceCorrect = totalPrice.replace('.', ',');
     setShowTotalPrice(totalPriceCorrect);
-    console.log(products);
   }, [cart]);
 
   useEffect(() => {
