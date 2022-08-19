654bee1805bb14f18c18c874b1298c7d234386f9
Vitor Correa <vitorcorrea18@gmail.com>
2022-08-19 10:57:55 -0300

    ref: apiSeller now is on the salesRequest.js file

--- front-end/src/api/salesRequest.js
+++ front-end/src/api/salesRequest.js
@@ -4,6 +4,12 @@ const api = axios.create({
   baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
 });
 
+export const apiSellers = async () => {
+  const endpoint = 'login/sellers';
+  const { data } = await api.get(endpoint);
+  return data;
+};
+
 export const createSale = async (body, token) => {
   const config = {
     headers: {
@@ -12,6 +18,12 @@ export const createSale = async (body, token) => {
   };
   const endpoint = '/sale';
   const { data } = await api.post(endpoint, body, config);
+  return data;
+};
+
+export const getSaleById = async (id) => {
+  const endpoint = `/sale/${id}`;
+  const { data } = await api.get(endpoint);
   return data;
 };
 
