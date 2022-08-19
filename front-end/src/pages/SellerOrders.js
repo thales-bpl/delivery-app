b803fafa26f2c26cf9f06216cb286947d6c64165
emmanoel1 <emmanoelporto@gmail.com>
2022-08-18 23:14:14 -0300

    feat: testIds commented

--- front-end/src/pages/SellerOrders.js
+++ front-end/src/pages/SellerOrders.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 
 export default function SellerOrders() {
-  // Fazer a verificação se o token é do vendedor caso contrario voltar a pagina de login
+  // - 48: seller_orders__element-order-id-<id>
+  // - 49: seller_orders__element-delivery-status-<id>
+  // - 50: seller_orders__element-order-date-<id>
+  // - 51: seller_orders__element-card-price-<id>
+  // - 52: seller_orders__element-card-address-<id></id>
 
   return (
     <div>
