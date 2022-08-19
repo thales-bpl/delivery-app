0b7bab6808c502cc767270f58841bfbde5346c0a
emmanoel1 <emmanoelporto@gmail.com>
2022-08-18 23:07:15 -0300

    update: Navbar now validate Token

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import getLocalStorage from '../services/getLocalStorage';
 
 export default function Navbar() {
-  const { name: userName } = JSON.parse(localStorage.getItem('user'));
+  // const { name: userName, role } = JSON.parse(localStorage.getItem('user'));
+
+  const [userStored, setUserStored] = useState({
+    userName: '',
+    role: '',
+  });
 
   const navigate = useNavigate();
+
+  const getUser = getLocalStorage('user');
+
+  useEffect(() => {
+    if (!getUser) {
+      navigate('/login');
+      console.log('voltou para login');
+    }
+
+    if (getUser) {
+      const { name, role } = getUser;
+      setUserStored({ userName: name, role });
+      console.log(`fez o login de ${role}`);
+    }
+  }, []);
 
   const logout = () => {
     localStorage.removeItem('user');
     navigate('/login');
   };
 
-  return (
+  const customerNavbar = (
     <nav>
       <button
         type="button"
@@ -30,7 +51,7 @@ export default function Navbar() {
         type="button"
         data-testid="customer_products__element-navbar-user-full-name"
       >
-        { userName }
+        { userStored.userName }
       </button>
       <button
         type="button"
@@ -41,4 +62,42 @@ export default function Navbar() {
       </button>
     </nav>
   );
+
+  const sellerNavbar = (
+    <nav>
+      <button
+        type="button"
+        data-testid="customer_products__element-navbar-link-orders"
+      >
+        PEDIDOS
+      </button>
+
+      <button
+        type="button"
+        data-testid="customer_products__element-navbar-user-full-name"
+      >
+        { userStored.userName }
+      </button>
+      <button
+        type="button"
+        data-testid="customer_products__element-navbar-link-logout"
+        onClick={ () => logout() }
+      >
+        quit
+      </button>
+    </nav>
+  );
+
+  // se o usuário for cliente, renderiza o navbar de cliente
+  if (userStored.role === 'customer') {
+    return customerNavbar;
+  }
+
+  // se usuário for vendedor, renderiza o navbar de vendedor
+  if (userStored.role === 'seller') {
+    return sellerNavbar;
+  }
+
+  // se não ..Loading..
+  return (<>...Loading...</>);
 }
--- front-end/src/pages/SellerOrders.js
+++ front-end/src/pages/SellerOrders.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 export default function SellerOrders() {
+  // Fazer a verificação se o token é do vendedor caso contrario voltar a pagina de login
+
   return (
     <div>
+      <Navbar />
       SellerOrders
     </div>
   );
