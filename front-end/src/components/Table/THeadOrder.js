4f0d98e2ca7bdd8daf33b45356777bdbbae1281c
Vitor Correa <vitorcorrea18@gmail.com>
2022-08-20 14:00:02 -0300

    feat: THeadOrder for order info

--- front-end/src/components/Table/THeadOrder.js
+++ front-end/src/components/Table/THeadOrder.js
@@ -1,10 +1,24 @@
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-// import { getSellerById } from '../../api/salesRequest';
+import { updateSale } from '../../api/salesRequest';
+import getLocalStorage from '../../services/getLocalStorage';
 
-export default function THeadOrder({ order }) {
-  const handleClick = () => {
-    console.log('funciona');
+export default function THeadOrder({ order, seller, setOrder }) {
+  const [role, setRole] = useState('');
+  const handleClick = async () => {
+    const updateStatus = { status: 'Entregue' };
+    const user = getLocalStorage('user');
+    const newData = await updateSale(order.id, updateStatus, user.token);
+    setOrder(newData);
   };
+
+  useEffect(() => {
+    const user = getLocalStorage('user');
+    setRole(user.role);
+  }, []);
+
+  const IdSellerName = 'customer_order_details__element-order-details-label-seller-name';
+  const IdStatus = 'customer_order_details__element-order-details-label-delivery-status';
 
   return (
     <thead className="table_head_order">
@@ -16,14 +30,11 @@ export default function THeadOrder({ order }) {
         </th>
 
         <th
-          data-testid={ `customer_order_details__
-          element-order-details-label-seller-name` }
+          data-testid={ IdSellerName }
         >
-          P. Vendedora: Fulana
-          {/* `P. Vendedora: ${getSellerById(order.sellerId)} ` */}
+          { `P. Vendedora: ${seller.name} ` }
         </th>
 
-        { /* Back tem que fazer uma rota /seller/:id */ }
         <th
           data-testid="customer_order_details__element-order-details-label-order-date"
         >
@@ -31,20 +42,32 @@ export default function THeadOrder({ order }) {
         </th>
 
         <th
-          data-testid={ `customer_order_details__
-          element-order-details-label-delivery-status` }
+          data-testid={ IdStatus }
         >
           {order.status}
         </th>
 
         <th>
-          <button
-            type="button"
-            data-testid="customer_order_details__button-delivery-check"
-            onClick={ handleClick }
-          >
-            Marcar como entregue
-          </button>
+          {role === 'Seller'
+            ? (
+              <button
+                type="button"
+                disabled={ order.status !== 'Pendente' }
+                data-testid="seller_order_details__button-delivery-check"
+                onClick={ handleClick }
+              >
+                Marcar como entregue
+              </button>)
+            : (
+              <button
+                type="button"
+                disabled
+                data-testid="customer_order_details__button-delivery-check"
+                onClick={ handleClick }
+              >
+                Marcar como entregue
+              </button>
+            )}
         </th>
       </tr>
     </thead>
@@ -52,6 +75,8 @@ export default function THeadOrder({ order }) {
 }
 
 THeadOrder.propTypes = {
+  setOrder: PropTypes.func.isRequired,
+  seller: PropTypes.instanceOf(Object).isRequired,
   order: PropTypes.shape({
     id: PropTypes.number,
     saleDate: PropTypes.string,
